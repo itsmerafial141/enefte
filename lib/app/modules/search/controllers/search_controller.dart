5ccf2ca9bc16159c01106578b2578dbe9c46e0cb
Rafi Fitra Alamsyah <rfitraalamsyah@gmail.com>
2022-03-24 15:54:10 +0700

    first commit

--- lib/app/modules/search/controllers/search_controller.dart
+++ lib/app/modules/search/controllers/search_controller.dart
@@ -1,4 +1,17 @@
 import 'package:get/get.dart';
 
 class SearchController extends GetxController {
+  final isFilled = false.obs;
+
+  // late TextEditingController searchController;
+
+  void searchCondition(String value) {
+    if (value.isNotEmpty) {
+      print("terisi : " + value);
+      isFilled.value = true;
+    } else {
+      print("Kozong : " + value);
+      isFilled.value = false;
+    }
+  }
 }
