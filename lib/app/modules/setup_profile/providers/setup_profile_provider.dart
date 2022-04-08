ce8278ae3221205465b73881c807754a2956fb0c
Rafi Fitra Alamsyah <rfitraalamsyah@gmail.com>
2022-04-08 17:04:34 +0700

    FIX API on srup_page and adding input validation with stateMixin

--- lib/app/modules/setup_profile/providers/setup_profile_provider.dart
+++ lib/app/modules/setup_profile/providers/setup_profile_provider.dart
@@ -1,47 +1,27 @@
-import 'dart:convert';
-
-import 'package:enefte/app/modules/setup_profile/responses/setup_profile_response.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class SetupProfileProvider extends GetConnect {
-  @override
-  void onInit() {
-    httpClient.baseUrl =
-        'https://firestore.googleapis.com/v1/projects/enefte-afeef/databases/(default)/documents';
-  }
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late CollectionReference users = firestore.collection('users');
 
-  Future<String> addUsers(
+  bool addUsers(
+    String image,
     String username,
     String email,
     String bio,
-  ) async {
-    final body = json.encode(
-      {
-        "fields": {
-          "username": {"stringValue": "1111"},
-          "email": {"stringValue": "1111"},
-          "bio": {"stringValue": "1111"}
-        }
-      },
-    );
-    final response = await post(
-      "https://firestore.googleapis.com/v1/projects/enefte-afeef/databases/(default)/documents/users?key=AIzaSyB8lYRVRG8XFRNMzsqxAUytVyNu8NNazps",
-      // MyStrings.url + "/users?key=" + MyStrings.api_key,
-      body,
-    );
-    // if (response.status.hasError) {
-    //   print(response.statusText.toString());
-    //   print("error bro!");
-    //   return Future.error(response.statusText.toString());
-    // } else {
-    //   print(response.bodyString.toString());
-    //   print("Success bro!");
-    //   return setupProfileResponseFromJson(response.bodyString.toString());
-    // }
-    if (response.bodyString.toString() != "null") {
-      return response.bodyString.toString();
-    } else {
-      return "Gagal hapus data!";
+  ) {
+    try {
+      users.add({
+        'profile_img': image,
+        'username': username,
+        'email': email,
+        'bio': bio,
+      });
+      return true;
+    } catch (err) {
+      print(err.toString());
+      return false;
     }
   }
 }
